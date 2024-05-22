fac1bd02e4d545e2fe15724e9362e0b53e2493ab
unknown <dorjeetsheyring0513@gmail.com>
2024-05-22 22:10:22 +0600

    Linked list

---
+++ Linked_list.py
@@ -0,0 +1,30 @@
+class Node:
+    def _init_(self, data):
+        self.data = data
+        self.next = None
+
+class Linkedlist: 
+    def _init_(self):
+        self.head = None
+
+    def insertEnd(self, newNode):
+        if self.head is None:
+            self.head = newNode
+        else:
+            lastNode = self.head
+            while True:
+                if lastNode.next is None:
+                    break
+                lastNode = lastNode.next
+            lastNode.next = newNode
+
+    def printlist(self):
+        if self.head is None:
+            print("List is empty")
+            return
+        currentNode = self.head
+        while True:
+            if currentNode is None:
+                break
+            print(currentNode.data)
+            currentNode = currentNode.next
